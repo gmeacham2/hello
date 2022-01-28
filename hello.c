86b599c94401fdb6225596b78ca1d01cf1ce9e2c
Grant Meacham <gmeacham2@huskers.unl.edu>
2022-01-28 15:44:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Grant Meacham
+ *         gmeacham2@huskers.unl.edu
+ * Date: 2022/28/01
+ *
+ * A simple Hello World! program in C
+ *
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+
+ int main(int argc,char **argv) {
+
+ printf("Grant Meacham\n");
+
+ return 0;
+}
